50d0234367686ad06d5ea3cfc674042e2bb9892d
Lamia Haider <lamia.haider@gmail.com>
2018-06-12 06:55:46 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,7 @@
 class Owner
   # code goes here
-  attr_reader :species, :name
-  attr_accessor :pets
+  attr_reader :species
+  attr_accessor :name, :pets
   @@all = []
 
   def initialize(species)
